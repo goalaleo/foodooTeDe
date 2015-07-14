1b5a4dff1a36e70ac1cc9fe1155737189382936c
goalaleo <leo.kiiski@helsinki.fi>
2015-07-14 16:10:49 +0300

    newest version of async

--- pollingDemo_async_req_server/www/js/app.js
+++ pollingDemo_async_req_server/www/js/app.js
@@ -14,11 +14,23 @@ pollingApp.run(function($ionicPlatform) {
 });
 //pollingApp.run end
 
+pollingApp.controller('CustomerController', ['$scope', 'customerFactory', function($scope, customerFactory) {
+    $scope.customers = customerFactory.getCustomers();
+
+    $scope.addCustomer = function() {
+        $scope.customers.push({
+            name: $scope.newCustomer.name,
+            city: $scope.newCustomer.city
+        });
+    }
+}]);
+// pollingApp.CustomerController end
+
 pollingApp.controller('MainController', ['$scope', 'Order','$timeout', function($scope, Order,$timeout) {
     // Get all orders
     (function tick() {
         $scope.orders = Order.query(function() {
-            $timeout(tick, 1000);
+            $timeout(tick, 5000);
         });
         $scope.orders.$promise.then(function(result) {
             $scope.orders = result;
@@ -42,3 +54,28 @@ pollingApp.factory('Order', ['$resource', function($resource) {
     return $resource("https://fodoo.herokuapp.com/orders.json");
 }]);
 // pollingApp.Order end
+
+pollingApp.factory('customerFactory', function() {
+    var factory = {};
+    var customers = [{
+        name: 'Dave Jones',
+        city: 'Phoenix'
+    }, {
+        name: 'Jamie Riley',
+        city: 'Atlanta'
+    }, {
+        name: 'Heedy Wahlin',
+        city: 'Chandler'
+    }, {
+        name: 'Thomas Winter',
+        city: 'Seattle'
+    }, {
+        name: 'Leo Kiiski',
+        city: 'Helsinki'
+    }];
+    factory.getCustomers = function() {
+        return customers;
+    }
+    return factory;
+
+});
